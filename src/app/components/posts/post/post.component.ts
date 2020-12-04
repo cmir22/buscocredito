78b83eca7a146fa86b16c410e055576e566ea998
Cruz Ibarra <jackzelda_vicio@hotmail.com>
2020-12-04 15:09:53 -0800

    Fixed some problems

--- src/app/components/posts/post/post.component.ts
+++ src/app/components/posts/post/post.component.ts
@@ -7,6 +7,10 @@ import { PostI } from 'src/app/shared/models/post.interface';
 import { UserI } from 'src/app/shared/models/user.interface';
 import { MatDialog } from '@angular/material/dialog';
 import { ModalComponent } from '../../../shared/component/modal/modal.component';
+import { AngularFirestore } from '@angular/fire/firestore';
+import * as firebase from 'firebase';
+
+
 
 @Component({
   selector: 'app-post',
@@ -15,15 +19,42 @@ import { ModalComponent } from '../../../shared/component/modal/modal.component'
 })
 export class PostComponent implements OnInit {
 
+  constructor(private route: ActivatedRoute, private postSvc: PostService, private db: AngularFirestore) { }
 
   public post$: Observable<PostI>;
-  constructor(private route: ActivatedRoute, private postSvc: PostService) { }
+  tipoPrestamo = "";
+
+  onLogout(): void {
+    firebase.auth().signOut().then(function () {
+    }).catch(function (error) {
+    });
+  }
+
+  setData(idPost) {
+    const formBid = document.querySelector('#formBid');
+    formBid.addEventListener('submit', (e) => {
+      e.preventDefault();
+      let bidBanco = (<HTMLInputElement>document.querySelector('#bidBanco')).value;
+      let bidTiempo = (<HTMLInputElement>document.querySelector('#bidTiempo')).value;
+      this.db.collection("posts").doc(`${idPost}`).update({
+        Banco: bidBanco,
+        tipoPrestamo: this.tipoPrestamo,
+        bidTiempo: bidTiempo
+      })
+        .then(function () {
+          console.log("Document successfully written!");
+        })
+        .catch(function (error) {
+          console.error("Error writing document: ", error);
+        });
+    })
+  }
 
   ngOnInit(): void {
     const idPost = this.route.snapshot.params.id;
     this.post$ = this.postSvc.getOnePost(idPost);
+    //this.setData(idPost)
+    this.setData(idPost);
   }
-
-
 
 }
